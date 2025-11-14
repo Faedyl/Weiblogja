0e697a000c628ee06d3594d6754980d1a64c69f8
Fadhil Awalia Kusuma <11231023@student.itk.ac.id>
2025-11-14 20:21:39 +0800

    feat: add Session Provider and Auth Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,40 +3,43 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Sidebar from "./components/sidebar/sidebar"
 import Navbar from "./components/navbar/navbar"
+import { AuthProvider } from "@/contexts/AuthContext"
+
+import { SessionProvider } from "next-auth/react"
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+        variable: "--font-geist-sans",
+        subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+        variable: "--font-geist-mono",
+        subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: "Weiblogja",
-	description: "Website Blog with Generative AI",
+        title: "Weiblogja",
+        description: "Web blog with Implementation Generative AI",
 };
 
 export default function RootLayout({
-	children,
+        children,
 }: Readonly<{
-	children: React.ReactNode;
+        children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable}`}>
-				<Navbar />
-				<div className="app-container">
-
-					<Sidebar />
-					<main className="main-content">
-
-
-						{children}
-					</main>
-				</div>
-			</body>
-		</html>
-	);
+        return (
+                <html lang="en">
+                        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+                                <SessionProvider>
+                                        <AuthProvider>
+                                                <Navbar />
+                                                <div className="app-container">
+                                                        <Sidebar />
+                                                        <main className="main-content">
+                                                                {children}
+                                                        </main>
+                                                </div>
+                                        </AuthProvider>
+                                </SessionProvider>                        </body>
+                </html>
+        );
 }
