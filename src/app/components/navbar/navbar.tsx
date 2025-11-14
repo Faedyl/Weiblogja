389b6ae7be3906daba3e524396e823ae3a43e110
Fadhil Awalia Kusuma <11231023@student.itk.ac.id>
2025-11-14 20:14:57 +0800

    refactor: change width searchbar , remove X

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -7,7 +7,11 @@ import { usePathname, useRouter } from 'next/navigation'
 import styles from './navbar.module.css'
 import SearchBar from '@/app/components/searchbar/searchbar'
 
-export default function Navbar() {
+interface NavbarProps {
+	onToggleSidebar?: () => void;
+}
+
+export default function Navbar({ onToggleSidebar }: NavbarProps) {
 	const [isMenuOpen, setIsMenuOpen] = useState(false)
 	const [searchQuery, setSearchQuery] = useState('')
 	const pathname = usePathname()
@@ -22,6 +26,13 @@ export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
 			<div className={styles.navContainer}>
+				<button
+					className={styles.sidebarToggleButton}
+					onClick={onToggleSidebar}
+					aria-label="Toggle sidebar"
+				>
+					<Menu size={24} />
+				</button>
 				{/* Logo/Brand */}
 				<Link href="/" className={styles.brand}>
 					<Image src="/weiblogja.svg" alt="Brand Logo" width={66} height={65} />
@@ -33,63 +44,7 @@ export default function Navbar() {
 					onResultClick={handleSearchResultClick}
 					className={styles.searchForm}
 				/>
-
-				{/* Mobile Menu Button */}
-				<button
-					className={styles.mobileMenuButton}
-					onClick={toggleMenu}
-					aria-label="Toggle menu"
-				>
-					{isMenuOpen ? <X size={24} /> : <Menu size={24} />}
-				</button>
 			</div>
-
-			{/* Mobile Menu */}
-			{isMenuOpen && (
-				<div className={styles.mobileMenu}>
-					{/* Mobile Search */}
-					<SearchBar
-						onResultClick={handleSearchResultClick}
-						className={styles.mobileSearchForm}
-						maxResults={4}
-					/>
-					{/* Mobile Links */}
-					<div className={styles.mobileNavLinks}>
-						<Link
-							href="/"
-							className={`${styles.mobileNavLink} ${pathname === '/' ? styles.active : ''}`}
-							onClick={() => setIsMenuOpen(false)}
-						>
-							<Home size={20} />
-							Home
-						</Link>
-						<Link
-							href="/library"
-							className={`${styles.mobileNavLink} ${pathname === '/library' ? styles.active : ''}`}
-							onClick={() => setIsMenuOpen(false)}
-						>
-							<LibraryBig size={20} />
-							Library
-						</Link>
-						<Link
-							href="/create"
-							className={`${styles.mobileNavLink} ${pathname === '/create' ? styles.active : ''}`}
-							onClick={() => setIsMenuOpen(false)}
-						>
-							<PlusCircle size={20} />
-							Create Blog
-						</Link>
-						<Link
-							href="/profile"
-							className={`${styles.mobileNavLink} ${pathname === '/profile' ? styles.active : ''}`}
-							onClick={() => setIsMenuOpen(false)}
-						>
-							<User size={20} />
-							Profile
-						</Link>
-					</div>
-				</div>
-			)}
 		</nav>
 	)
 }
