0b817259df6eff5a73b6b870690b8c02229aa673
Fadhil Awalia Kusuma <11231023@student.itk.ac.id>
2025-12-24 15:51:35 +0800

    chore: update next package

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
         serverExternalPackages: ['@napi-rs/canvas', 'pdfjs-dist'],
+        experimental: {
+
+                serverComponentsExternalPackages: ['@napi-rs/canvas', 'pdfjs-dist'],
+        },
 
         images: {
                 remotePatterns: [
